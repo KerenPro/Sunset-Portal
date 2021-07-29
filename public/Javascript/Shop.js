fbed1ed1e1c7dd8d20e44388fd16ee64ee55cda0
Keren Abramson <keren.abramson@gmail.com>
2021-07-29 18:57:27 +0300

    itteration works

--- public/Javascript/Shop.js
+++ public/Javascript/Shop.js
@@ -1,24 +1,27 @@
 let db = firebase.firestore();
 let stockRef = db.collection("Stock");
 let boardsDocRef = stockRef.doc("Boards");
+let menSuitsDocRef = stockRef.doc("men-suits");
+let womenSuitsDocRef = stockRef.doc("women-suits");
 
+let recommendationDIV = document.getElementById("recommends");
 stockRef.get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data()["1"].recommend);
-    });
-});
-
-stockRef.get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        for (let i=1 ; i<=2 ; i++){
+        console.log(doc.id)
+        try {
+            for (let i=1 ; i<=3 ; i++){
             console.log(doc.data()[i].recommend);
+            if (doc.data()[i].recommend === true){
+                console.log(doc.data()[i].productName)
+            }
         }
-        // doc.data() is never undefined for query doc snapshots
-        // console.log(doc.id, " => ", doc.data()["1"].recommend);
+    } catch(error) {
+            console.log("document number doesn't exist")
+        };
     });
 });
 
+/***********************************THIS HELPS ME **************************************/
 // boardsDocRef.get().then((doc) => {
 //     if(doc.data().boardOne.recommend === true){
 //         console.log("I am number 1!")
@@ -46,3 +49,20 @@ stockRef.get().then((querySnapshot) => {
 //     .catch((error) => {
 //         console.log("Error getting documents: ", error);
 //     });
+
+// stockRef.get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         // doc.data() is never undefined for query doc snapshots
+//         console.log(doc.id, " => ", doc.data()["1"].recommend);
+//     });
+// });
+
+// stockRef.get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         for (let i=0 ; i<=1 ; i++){
+//             console.log(doc.data()[i].recommend);
+//         }
+//         // doc.data() is never undefined for query doc snapshots
+//         // console.log(doc.id, " => ", doc.data()["1"].recommend);
+//     });
+// });
