0b8a6b328414d5313f4a86b584b3d966399f1a6d
Keren Abramson <keren.abramson@gmail.com>
2021-07-29 17:21:22 +0300

    access data in documents

--- public/Javascript/Shop.js
+++ public/Javascript/Shop.js
@@ -2,26 +2,47 @@ let db = firebase.firestore();
 let stockRef = db.collection("Stock");
 let boardsDocRef = stockRef.doc("Boards");
 
-boardsDocRef.get().then((doc) => {
-    if(doc.data().1
-    .recommend === true){
-        console.log("I am number 1!")
-    } else {
-        console.log ("I failed")
-    }
-    if (doc.exists) {
-        console.log("Document data:", doc.data());
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-}).catch((error) => {
-    console.log("Error getting document:", error);
+stockRef.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data()["1"].recommend);
+    });
 });
 
-// let staffRecommendations = document.getElementById("recommendations");
-// boardsDocRef.get().then((doc) => {
-    
-// }
+stockRef.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        for (let i=1 ; i<=2 ; i++){
+            console.log(doc.data()[i].recommend);
+        }
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data()["1"].recommend);
+    });
+});
 
-// )
+// boardsDocRef.get().then((doc) => {
+//     if(doc.data().boardOne.recommend === true){
+//         console.log("I am number 1!")
+//     } else {
+//         console.log ("I failed")
+//     }
+//     if (doc.exists) {
+//         console.log("Document data:", doc.data());
+//     } else {
+//         // doc.data() will be undefined in this case
+//         console.log("No such document!");
+//     }
+// }).catch((error) => {
+//     console.log("Error getting document:", error);
+// });
+
+// stockRef.where("recommend", "==", true)
+//     .get()
+//     .then((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//             // doc.data() is never undefined for query doc snapshots
+//             console.log(doc.id, " => ", doc.data());
+//         });
+//     })
+//     .catch((error) => {
+//         console.log("Error getting documents: ", error);
+//     });
