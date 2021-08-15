933f14ed0755f003f10905278b6844c17b6e7957
Keren Abramson <keren.abramson@gmail.com>
2021-08-15 09:52:52 +0300

    Recommendations done

--- public/Javascript/Order.js
+++ public/Javascript/Order.js
@@ -6,3 +6,39 @@ const stockRef = db.collection("Stock");
 
 const item = document.getElementById("item1");
 
+
+
+
+
+/*****I NEED THIS****/
+// Add a new document in collection "cities"
+// db.collection("cities").doc("LA").set({
+//     name: "Los Angeles",
+//     state: "CA",
+//     country: "USA"
+// })
+//     .then(() => {
+//         console.log("Document successfully written!");
+//     })
+//     .catch((error) => {
+//         console.error("Error writing document: ", error);
+//     });
+
+//Data types
+// var docData = {
+//     stringExample: "Hello world!",
+//     booleanExample: true,
+//     numberExample: 3.14159265,
+//     dateExample: firebase.firestore.Timestamp.fromDate(new Date("December 10, 1815")),
+//     arrayExample: [5, true, "hello"],
+//     nullExample: null,
+//     objectExample: {
+//         a: 5,
+//         b: {
+//             nested: "foo"
+//         }
+//     }
+// };
+// db.collection("data").doc("one").set(docData).then(() => {
+//     console.log("Document successfully written!");
+// });
