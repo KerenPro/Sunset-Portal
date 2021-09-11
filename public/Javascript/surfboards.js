44840f63cca79e9fd4960e81cc635056f40829a1
jeniabartal <88040547+jeniabartal@users.noreply.github.com>
2021-09-11 19:15:08 +0300

    validations, change calendar section

--- public/Javascript/surfboards.js
+++ public/Javascript/surfboards.js
@@ -75,7 +75,7 @@ function color(){
 	 }
    }
 }
-      //connect to Google Calendar API with credantials(api key and client ID)//
+		//connect to Google Calendar API with credantials(api key and client ID)//
       //API key from the Google Develoepr Console - to handle any unauthenticated
       // requests in the code.
     
@@ -87,7 +87,8 @@ function color(){
       var clientId = '121390106151-e6s4een21lsq4f9jdj49p2h6bc0ng7sj.apps.googleusercontent.com';
       var scopes = 'https://www.googleapis.com/auth/calendar';
 
-      // The Calendar entry to create
+      // The Calendar entry to create//
+	  
 
       function handleClientLoad() {
         gapi.load('client', initClient);
@@ -107,39 +108,42 @@ function color(){
 	  
 		//API request//
       function makeRequest(resource) {
-
-        gapi.client.request({
-          'path': '/calendar/v3/calendars/sunsetsurfportal@gmail.com/events',
-          'method': 'POST',
-          'body': resource
-        }).then(function(resp) {
-          writeResponse(resp.result);
-        });
+		  gapi.auth2.getAuthInstance().signIn({prompt:'select_account'}).then((res)=>{
+			console.log(res);
+			gapi.client.request({
+			  'path': '/calendar/v3/calendars/primary/events',
+			  'method': 'POST',
+			  'body': resource
+			}).then(function(resp) {
+				$('#event-id').val(""+ resp.result.id);
+				console.log("from resp api");
+				console.log(resp.result.id);
+			  writeResponse(resp.result);
+			});
+		  }).catch((res)=>{
+			 console.log("google login failed");
+			 console.log(res); 
+		  });
       }
-		//This section code create a link to created event - just for check if it work correctly -רק לצורך בדיקה לשריון ביומן //
-		//זה לא היהי בסופי //
+		//This section code create a link to created event  //
+
       function writeResponse(response) {
         console.log(response);
         var creator = response.creator.email;
         var calendarEntry = response.htmlLink;
         var infoDiv = document.getElementById('info');
         var infoMsg = document.createElement('P');
-        infoMsg.appendChild(document.createTextNode('Calendar entry ' +
-            'successfully created by ' + creator));
+        infoMsg.appendChild(document.createTextNode('האירוע ' +
+            'נוצר בהצלחה ע"י ' + creator));
         infoDiv.appendChild(infoMsg);
         var entryLink = document.createElement('A');
         entryLink.href = calendarEntry;
         entryLink.appendChild(
-            document.createTextNode('View the Calendar entry'));
+            document.createTextNode('צפה באירוע שנוצר ביומנך'));
         infoDiv.appendChild(entryLink);
       }
 
    
-
-
-
-
-
 
 $(document).ready(function () {
 	loadDoc();
@@ -147,10 +151,10 @@ $(document).ready(function () {
 	$('#order-form').on('submit', function (event) {
 			event.preventDefault();
 			
-			//validation//
+			//form validation//
 			let isError = false;
 			if($('#phone').val() === ""){
-				$('#errphone').html("Phone can't be empty");
+				$('#errphone').html("יש להזין מספר טלפון");
 				isError = true;
 				console.log ("inside Submit");
 			}
@@ -158,32 +162,101 @@ $(document).ready(function () {
 				
 				$('#errphone').html("");
 			}
-		//more validation for other fields//
 		
+			if($('#fname').val() === ""){
+				$('#errfname').html("יש להזין שם פרטי");
+				isError = true;
+			}
+			else{
+				
+				$('#errfname').html("");
+			}
+			if($('#lname').val() === ""){
+				$('#errlname').html("יש להזין שם משפחה");
+				isError = true;
+			}
+			else{
+				
+				$('#errlname').html("");
+			}
+		    if($('#taz').val() === ""){
+				$('#errtaz').html("יש להזין תעודת זהות");
+				isError = true;
+			}
+			else{
+				
+				$('#errtaz').html("");
+			}
+			if($('#leida').val() === ""){
+				$('#errleida').html("יש להזין תאריך לידה");
+				isError = true;
+			}
+			else{
+				
+				$('#errleida').html("");
+			}
+			if($('#taarih-azmana').val() === ""){
+				$('#errtaarih-azmana').html("יש לבחור תאריך השכרה");
+				isError = true;
+			}
+			else{
+				
+				$('#errtaarih-azmana').html("");
+			}
 		
-		
-		
-		
-		
+			if($('#from').val() === ""){
+				$('#errfrom').html("יש לבחור שעת התחלה");
+				isError = true;
+			}
+			else{
+				
+				$('#errfrom').html("");
+			}	
+				
+			if($('#mida').val() === ""){
+				$('#errmida').html("יש לבחור מידה");
+				isError = true;
+			}
+			else{
+				
+				$('#errmida').html("");
+			}
+			
+			if (!($('#azhara').prop('checked'))){
+				$('#err-azhara').html("יש לסמן הצהרת בריאות");
+				isError = true;
+			}
+			else{
+				
+				$('#err-azhara').html("");
+			}			
+					
+			////	/////
 				
 			if(isError){
 				
 				return;
 			}
 			
-			//אם כל הולידציות תקינות אז תתבצע שליחה לשרת ושריון ביומן גוגל של בית העסק//
+			
+			
+			// אם הולדיציות תקינות תתבצע שליחת הזמנה ובנוסףב מידה והמשתמש יבחר שריון ביומן גוגל האישי שלו//
 			
 			// שריון ביומן//
 			//יצירת משתנים לשליחה ליומן//
-			 let summary = $('#phone').val() +" " + $('#phone').val() +""+$('#phone').val()+""+$('#phone').val();
+			///////////////////////
+			//אם המשתמש סימן שהוא רוצה לשמור את האירוע ביון אז יתבצע //
+			if($('#calendar').prop('checked') ){
+			 console.log("writing to google calendar");
+			  let summary = (" השכרה במועדון גלישה SUNSET , ברחוב בן גוריון 162 עבור: ") + " " +$('#parit').val() + " " + ("מידה:") + " " + $('#mida').val() ;
 			  var supportDate   = document.getElementById("taarih-azmana");
 			  var eventStart = document.getElementById("from");  
-			  var eventEnd = document.getElementById("to");
-			  var start = supportDate.value +"T"+ eventStart.value +":00.000+03:00";
-			  var end = supportDate.value +"T"+ eventEnd.value +":00.000+03:00";
+			  var eventEnd = ("0" + eventStart.value).slice(-2);
+			  var start = supportDate.value +"T"+  eventStart.options[eventStart.selectedIndex].text+":00.000+03:00";
+			  var end = supportDate.value +"T"+ eventEnd +":00:00.000+03:00";
 			  var resource = {
 				"summary": summary,
-				"location": "Israel",
+				"location":  "Bat Yam, Israel",
 				"end": {"dateTime": end},
 				 "start": {"dateTime": start }
 			 
@@ -191,8 +264,13 @@ $(document).ready(function () {
 			  console.log('start: ' + start)
 			  console.log('end: ' + end)
 			  makeRequest(resource);
-			  
-			  //prepare data
+			}
+			else{
+				console.log("no calendar use");
+			}
+			
+	
+			  //prepare data to send //
 			  const data = {};
 			  const items=[];
 			  
@@ -200,9 +278,10 @@ $(document).ready(function () {
 			  items.push( "גלשן גלים");
 			  // check if customer wants also suite//
 			//אם הוא לא מסומן "ללא חליפה" אז אני עושה : 
-			 items.push( "הערך של הסלקטור");
+		     items.push( "הערך של הסלקטור");
 			 data.itemTypes = items;
 			 
+			 data.calendarEventId= $('#event-id').val();
 			 
 			 
 			 
