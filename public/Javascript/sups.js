c05bb790d1a43af6484b9cfeb73fd12a984f726b
Keren Abramson <keren.abramson@gmail.com>
2021-09-19 13:11:28 +0300

    updated sups and surfboard orders

--- public/Javascript/sups.js
+++ public/Javascript/sups.js
@@ -142,6 +142,9 @@ function makeRequest(resource) {
           console.log("from resp api");
           console.log(resp.result.id);
           writeResponse(resp.result);
+        })
+        .then((res) => {
+          addOrder();
         });
     })
     .catch((res) => {
@@ -274,7 +277,10 @@ $(document).ready(function () {
       console.log("no calendar use");
     }
 
-    window.setTimeout(addOrder, 10000);
+    const addCalendar = document.getElementById("calendar");
+    if (!addCalendar.checked) {
+      addOrder();
+    }
   });
 });
 
