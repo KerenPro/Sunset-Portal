185a955e37cbc8b4ccd53748b97e23ab9842cb1d
Keren Abramson <keren.abramson@gmail.com>
2021-09-27 20:25:20 +0300

    Rent main page redirection

--- public/Javascript/userAuth.js
+++ public/Javascript/userAuth.js
@@ -1,6 +1,17 @@
 //variable
 const auth = firebase.auth();
 
+/**Helper functions for Ifs**/
+//User is signed out
+const signedOutRedirect = () => {
+  //Homepage
+  if (window.location.pathname === "/Sunset-Portal/public/index.html") {
+    window.location.href = "./Includes/signUpSignIn.html";
+  } else {
+    window.location.href = "./signUpSignIn.html";
+  }
+};
+
 //check user auth state
 const privateCheckUser = () => {
   console.log("I'm inside the check Auth User");
@@ -10,10 +21,18 @@ const privateCheckUser = () => {
     if (user) {
       const userEmail = user.email;
       console.log(`Sign in user is ${userEmail}`);
-      window.location.href = "./Includes/private.html";
+      if (window.location.pathname === "/Sunset-Portal/public/index.html") {
+        window.location.href = "./Includes/private.html";
+      }
+      if (
+        window.location.pathname ===
+        "/Sunset-Portal/public/Includes/RentMainPage.html"
+      ) {
+        window.location.href = "./private.html";
+      }
     } else {
       console.log("I need to redirect");
-      window.location.href = "./Includes/signUpSignIn.html";
+      signedOutRedirect();
     }
   });
 };
