f28b1e057be50feaccac1566167d05b74242f9c5
Keren Abramson <keren.abramson@gmail.com>
2021-09-28 12:54:52 +0300

    seperate page for index.html

---
+++ public/Javascript/userAuthIndex.js
@@ -0,0 +1,75 @@
+//variable
+const auth = firebase.auth();
+const redirectToHomepage = () => {
+  window.location.href = "../index.html";
+};
+
+/**check user Authentication and Redirect**/
+//Private redirection
+const privateNav = document.getElementById("private-link");
+privateNav.addEventListener("click", (event) => {
+  console.log("I'm inside the check Auth User");
+  const user = auth.currentUser;
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      const userEmail = user.email;
+      console.log(`Sign in user is ${userEmail}`);
+      window.location.href = "./Includes/private.html";
+    } else {
+      console.log("I need to redirect");
+      window.location.href = "./Includes/signUpSignIn.html";
+    }
+  });
+});
+
+//Lessons redirection
+const lessonsNav = document.getElementById("lessons-link");
+lessonsNav.addEventListener("click", (event) => {
+  console.log("I'm inside the check Auth User");
+  const user = auth.currentUser;
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      const userEmail = user.email;
+      console.log(`Sign in user is ${userEmail}`);
+      window.location.href = "./Includes/Lessons.html";
+    } else {
+      console.log("I need to redirect");
+      window.location.href = "./Includes/signUpSignIn.html";
+    }
+  });
+});
+
+//Rent Main Page redirection
+const rentHomepageNav = document.getElementById("rent-homepage-link");
+rentHomepageNav.addEventListener("click", (event) => {
+  console.log("I'm inside the check Auth User");
+  const user = auth.currentUser;
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      const userEmail = user.email;
+      console.log(`Sign in user is ${userEmail}`);
+      window.location.href = "./Includes/RentMainPage.html";
+    } else {
+      console.log("I need to redirect");
+      window.location.href = "./Includes/signUpSignIn.html";
+    }
+  });
+});
+
+/**Sign Out**/
+const signOut = () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      alert("התנתקת בהצלחה");
+      redirectToHomepage();
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+};
+//updating for firebase
